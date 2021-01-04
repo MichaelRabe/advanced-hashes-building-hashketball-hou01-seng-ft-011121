aad8660112190f0c5f9d1b5b3b61e281d7ab2bdc
Michael Rabe <m.trabe96@gmail.com>
2021-01-04 20:33:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -14,7 +14,7 @@ def game_hash
       :assists => 12, 
       :steals => 3, 
       :blocks => 1, 
-      :slam_dunks => 1}
+      :slam_dunks => 1},
       {:player_name => "Reggie Evans", 
       :number => 30, 
       :shoe => 14, 
@@ -23,7 +23,7 @@ def game_hash
       :assists => 12, 
       :steals => 12, 
       :blocks => 12, 
-      :slam_dunks => 7}
+      :slam_dunks => 7},
       {:player_name => "Brook Lopez", 
       :number => 11, 
       :shoe => 17, 
@@ -32,7 +32,7 @@ def game_hash
       :assists => 10, 
       :steals => 3, 
       :blocks => 1, 
-      :slam_dunks => 15}
+      :slam_dunks => 15},
       {:player_name => "Mason Plumlee", 
       :number => 1, 
       :shoe => 19, 
@@ -41,7 +41,7 @@ def game_hash
       :assists => 6, 
       :steals => 3, 
       :blocks => 8, 
-      :slam_dunks => 5}
+      :slam_dunks => 5},
       {:player_name => "Jason Terry", 
       :number => 31, 
       :shoe => 15, 
@@ -64,7 +64,7 @@ def game_hash
       assists: 1,
       steals: 2,
       blocks: 7,
-      slam_dunks: 2}
+      slam_dunks: 2},
       {player_name: "Bismack Biyombo", 
       number: 0, 
       shoe: 16,
@@ -73,7 +73,7 @@ def game_hash
       assists: 7,
       steals: 22,
       blocks: 15,
-      slam_dunks: 10}
+      slam_dunks: 10},
       {player_name: "DeSagna Diop",
       number: 2,
       shoe: 14,
@@ -82,7 +82,7 @@ def game_hash
       assists: 12,
       steals: 4,
       blocks: 5,
-      slam_dunks: 5}
+      slam_dunks: 5},
       {player_name: "Ben Gordon",
       number: 8,
       shoe: 15,
@@ -91,7 +91,7 @@ def game_hash
       assists: 2,
       steals: 1,
       blocks: 1,
-      slam_dunks: 0}
+      slam_dunks: 0},
       {player_name: "Kemba Walker",
       number: 33,
       shoe: 15,
