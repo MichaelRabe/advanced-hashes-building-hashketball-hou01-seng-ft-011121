fa03f41f381bda5eb7ded5ebf0021648e0daf909
Michael Rabe <m.trabe96@gmail.com>
2021-01-04 20:10:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,17 @@
 require 'pry'
 
 def game_hash = {
-  :home => :
+  :home => {:team_name => "Brooklyn Nets", :colors => ["black", "white"], :players => [:player_name =>, 
+    :number =>, 
+    :shoe =>, 
+    :points =>, 
+    :rebounds =>, 
+    :assists =>, 
+    :steals =>, 
+    :blocks =>, 
+    :slam_dunks =>]
+    :player_name =>, :number =>, :shoe =>, :points =>, :rebounds =>, :assists =>, :steals =>, :blocks =>, :slam_dunks =>
+  }
+  
   :away
 }
